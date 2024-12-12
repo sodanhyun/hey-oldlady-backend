698402a885650be0bf438eddadf974464047fcb6
danhyeon <sodanhyun@gmail.com>
2024-12-12 10:36:17 +0900

    init

---
+++ src/test/java/com/goormthon/halmang/HalmangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.halmang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalmangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
